d0c9d6d31182efa48d38298fc3f51dbe52baf8a8
unknown <milosst20@gmail.com>
2022-06-22 16:14:29 +0200

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+console.log("Client side javascript is loaded");
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+
+// messageOne.textContent = "";
+
+weatherForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const location = search.value;
+
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+
+  fetch("http://localhost:3000/weather?address=" + search.value).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          console.log(data.error);
+          messageOne.textContent = data.error;
+        } else {
+          console.log(data.location);
+          messageOne.textContent = data.location;
+          console.log(data.forecast);
+          messageTwo.textContent = data.forecast;
+        }
+      });
+    }
+  );
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,38 @@
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=24cde5c867aeee3cdf123b76777dab25&query=" +
+    latitude +
+    "," +
+    longitude;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to location services!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location. Try another search", undefined);
+    } else {
+      callback(
+        undefined,
+        "It is currently " +
+          body.current.temperature +
+          " degrees and it feels like " +
+          body.current.feelslike +
+          " degrees out"
+      );
+    }
+  });
+};
+
+module.exports = forecast;
